57700f1cab81a0d96261501856aa6cd19cb3a694
dell <2644280881@qq.com>
2023-07-04 10:45:21 +0800

    修改跳转路径

--- TSSP.web/Controllers/EnterpriseController.cs
+++ TSSP.web/Controllers/EnterpriseController.cs
@@ -14,6 +14,11 @@ namespace TSSP.web.Controllers
             ViewBag.Message = "Your application description page.";
             return View();
         }
-    
+
+        public ActionResult CompanyProfile()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
     }
 }
